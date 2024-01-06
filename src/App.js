954f623a7f0279ccf3a76f8113af34aafbb2e0f9
Gomolemo <gomolemo.mpyana@gmail.com>
2024-01-06 17:18:34 +0200

    Home content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Menu from './components/Menu';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <Menu/>
+      <Home/>
     </div>
   );
 }
