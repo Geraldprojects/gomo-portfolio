be34af0c2bbf82e1e01e36e9da59371690047812
Gomolemo <gomolemo.mpyana@gmail.com>
2024-01-13 23:24:25 +0200

    css modules addition

--- src/components/Homev1/Home.js
+++ src/components/Homev1/Home.js
@@ -3,11 +3,12 @@ import GithubIcon from "./github.svg";
 import EmailIcon from "./mail.svg";
 import LinkedInIcon from "./linkedin.svg";
 import SalesforceIcon from "./salesforce.svg";
+import Styles from './Home.module.css';
 
 const Home = () => {
   return (
-    <div className="Home-container">
-      <section className="home-content">
+    <div className={Styles["Home-container"]}>
+      <section className={Styles["Home-content"]}>
         <h1>Hi, my name is</h1>
         <h2>Gomolemo Mpyana</h2>
         <h3>I build things for the web.</h3>
@@ -19,25 +20,25 @@ const Home = () => {
           totam tempora
         </p>
 
-        <button className="home-action">Let's work!</button>
+        <button className= {Styles["home-action"]}>Let's work!</button>
       </section>
 
-      <aside className="contacts">
-        <ul>
+      <aside className={Styles.contacts}>
+        <ul className={Styles["contacts-list"]}>
           <li>
-            <img className="Contact-icon" src={GithubIcon} alt="" />
+            <img className={Styles["Contact-icon"]} src={GithubIcon} alt="" />
           </li>
           <li>
-            <img className="Contact-icon email" src={EmailIcon} alt="" />
+            <img className={Styles["Contact-icon"]} src={EmailIcon} alt="" />
           </li>
           <li>
-            <img className="Contact-icon" src={GithubIcon} alt="" />
+            <img className={Styles["Contact-icon"]}src={GithubIcon} alt="" />
           </li>
           <li>
-            <img className="Contact-icon" src={LinkedInIcon} alt="" />
+            <img className={Styles["Contact-icon"]} src={LinkedInIcon} alt="" />
           </li>
           <li>
-            <img className="Contact-icon" src={SalesforceIcon} alt="" />
+            <img className={Styles["Contact-icon"]} src={SalesforceIcon} alt="" />
           </li>
         </ul>
       </aside>
