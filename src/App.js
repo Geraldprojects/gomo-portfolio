87215efeae38cdbac0f14185fa005cab8bc11b84
Gomolemo <gomolemo.mpyana@gmail.com>
2024-01-17 18:21:20 +0200

    Experience

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import Menu from './components/Menu';
 import Home2 from './components/Home/Homev2';
+import Experience from './components/Experience/Experience';
 /* import Home from './components/Homev1/Home'; */
 
 function App() {
+  
   return (
     <div className="App">
       <Menu/>
       <Home2/>
+      <Experience/>
     </div>
   );
 }
